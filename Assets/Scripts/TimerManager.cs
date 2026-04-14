8122bfc12af181fbd3015c144fb2e94495659751
Andréa.C <155273473+Andie4@users.noreply.github.com>
2026-04-14 21:15:38 +0200

    ui corrections and add effect stars

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using TMPro;
+using sceneManagement = UnityEngine.SceneManagement;
 
 public class TimerManager : MonoBehaviour
+
 {
     public float time; 
     public TextMeshProUGUI timerText;
@@ -12,5 +14,12 @@ public class TimerManager : MonoBehaviour
     {
         time += Time.deltaTime;
         timerText.text = time.ToString("F1"); 
+
+        // récupérer le temps du level 1 sur la deuxième scène 
+        if (sceneManagement.SceneManager.GetActiveScene().name == "Level 2")
+        {
+            float timeLevel1 = PlayerPrefs.GetFloat("TimeLevel1", 0f);
+            timerText.text = "Time Level 1: " + timeLevel1.ToString("F1") + "s";
+        }
     }
 }
